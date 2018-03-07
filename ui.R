e731dfd7ab975d4d531aa92b414f10cbb883975b
Andrew Nguyen <nguyea6@uw.edu>
2018-03-06 16:05:21 -0800

    add combined pedestrian and bicycle data set

--- ui.R
+++ ui.R
@@ -14,16 +14,20 @@ ui <- fluidPage(
     tabPanel("Introduction",
       h3("Summary:"),
       p("Our team's data report will attempt to present data relating to traffic around Seattle and how that traffic may be affected by various factors such as day of the week, season, and weather."),
-      h3("Inspiration:"),
-      pre("Inspiration goes here"),
+      h3("Motivation:"),
+      pre("Motivation goes here"),
       h3("Limitations:"),
       pre("Limitations go here.")
     ),
-    
+
     tabPanel("Map",
-      pre("Map goes here")
+      pre("Heat map goes here")
     ),
-    
+
+    tabPanel("Graph",
+      pre("Graph of traffic vols per street vs date goes here")
+    )
+
     tabPanel("Analysis",
       pre("Analysis goes here")
     )
