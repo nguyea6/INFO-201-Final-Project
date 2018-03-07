92b2df48a7a50c209420b1f0e1c0193ecc4a77e2
Andrew Nguyen <nguyea6@uw.edu>
2018-03-06 19:03:53 -0800

    add weekday col to data frame

--- data_processing.R
+++ data_processing.R
@@ -1,24 +1,37 @@
 # data_processing.R: Contains common variables and functions
 
 traffic.greenway <- read.csv("data/bicycle/NW_58th_St_Greenway_at_22nd_Ave_NW_Bike_Counter.csv", stringsAsFactors = FALSE) %>%
-  select("Date", "NW.58th.St.Greenway.st.22nd.Ave.NW.Total")
+  select("Date", "NW.58th.St.Greenway.st.22nd.Ave.NW.Total") %>%
+  #mutate(lat = 47.6709191, long = -122.3869181)
 
 traffic.fremont.bridge <- read.csv("data/bicycle/Fremont_Bridge_Hourly_Bicycle_Counts_by_Month_October_2012_to_present.csv", stringsAsFactors = FALSE) %>%
   mutate(Fremont.Bridge.Sidewalk = Fremont.Bridge.West.Sidewalk + Fremont.Bridge.East.Sidewalk) %>%
-  select("Date", "Fremont.Bridge.Sidewalk")
+  select("Date", "Fremont.Bridge.Sidewalk") %>%
+  #mutate(lat = 47.6475338, long = -122.3519361)
 
 traffic.elliot.bay.trail <- read.csv("data/bicycle/Elliott_Bay_Trail_in_Myrtle_Edwards_Park.csv", stringsAsFactors = FALSE) %>%
   select("Date", "Elliott.Bay.Trail.in.Myrtle.Edwards.Park.Total")
+  #mutate(lat = 47.6080618, long = -122.3447558)
+
 traffic.burke.gilman.trail <- read.csv("data/bicycle/Burke_Gilman_Trail_north_of_NE_70th_St_Bike_and_Ped_Counter.csv", stringsAsFactors = FALSE) %>%
-  select("Date", "BGT.North.of.NE.70th.Total")
+  select("Date", "BGT.North.of.NE.70th.Total") %>%
+  #mutate(lat = 47.6799869, long =-122.2655913)
+
 traffic.broadway <- read.csv("data/bicycle/Broadway_Cycle_Track_North_Of_E_Union_St.csv", stringsAsFactors = FALSE) %>%
-  select("Date", "Broadway.Cycle.Track.North.Of.E.Union.St.Total")
+  select("Date", "Broadway.Cycle.Track.North.Of.E.Union.St.Total") %>%
+  #mutate(lat = 47.6132426, long =-122.3211899)
+
 traffic.39th.ave <- read.csv("data/bicycle/39th_Ave_NE_Greenway_at_NE_62nd_St.csv", stringsAsFactors = FALSE) %>%
-  select("Date", "X39th.Ave.NE.Greenway.at.NE.62nd.St.Total")
+  select("Date", "X39th.Ave.NE.Greenway.at.NE.62nd.St.Total") %>%
+  #mutate(lat = 47.6739715, long =-122.2863352)
+
 traffic.26th.ave <- read.csv("data/bicycle/26th_Ave_SW_Greenway_at_SW_Oregon_St.csv", stringsAsFactors = FALSE) %>%
-  select("Date", "X26th.Ave.SW.Greenway.at.SW.Oregon.St.Total")
+  select("Date", "X26th.Ave.SW.Greenway.at.SW.Oregon.St.Total") %>%
+  #mutate(lat = 47.562903, long = -122.3676473)
+
 traffic.2nd.ave <- read.csv("data/bicycle/2nd_Ave_Cycle_Track_North_of_Marion_St.csv", stringsAsFactors = FALSE) %>%
-  select("Date", "X2nd.Ave.Cycletrack")
+  select("Date", "X2nd.Ave.Cycletrack") %>%
+  #mutate(lat = 47.604445, long =-122.3352528)
 
 traffic.ped.bicycle <-
   full_join(traffic.greenway, traffic.fremont.bridge, by = "Date") %>%
@@ -29,8 +42,13 @@ traffic.ped.bicycle <-
   full_join(traffic.26th.ave, by = "Date") %>%
   full_join(traffic.2nd.ave, by = "Date") %>%
   separate(Date, into = c("Date", "Time", "AMPM"), sep = " ") %>%
-  mutate(Time = paste(Time, AMPM)) %>%
-  select("Date", "Time",
+  mutate(
+    Time = paste(Time, AMPM),
+    Weekday = weekdays(as.Date(Date)) #https://stackoverflow.com/a/9216316
+  ) %>%
+  select(
+      #"lat", "long",
+    "Date", "Weekday", "Time",
     "NW.58th.St.Greenway.st.22nd.Ave.NW.Total",
     "Fremont.Bridge.Sidewalk",
     "Elliott.Bay.Trail.in.Myrtle.Edwards.Park.Total",
